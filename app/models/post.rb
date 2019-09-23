a41b46e63ede953081c3b261b9cad57a3080dc64
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-23 01:11:25 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
   before_save :make_title_case
-  
+
   private
 
   def is_title_case
