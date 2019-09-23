5ce2109d86e109bd4669c6bd127113030387c198
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-23 01:23:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:index, :new, :show, :edit, :create]
+  patch '/posts/:id', to 'posts#update'
+  resources :routes, only: [:index, :new, :show, :edit, :create]
+  patch '/routes/:id', to 'routes#update'
 end
